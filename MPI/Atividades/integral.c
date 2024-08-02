6c62c6c5a1c078e24b520d416518f716a974f38b
Ale Santos <71612455+ale-st@users.noreply.github.com>
2024-08-02 14:04:51 -0300

    Update integral.c

--- MPI/Atividades/integral.c
+++ MPI/Atividades/integral.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>  
+#include <mpi.h>
 #include <math.h>
 
 // função q representa f(x)= x^2 - 4x + 8
@@ -22,8 +24,53 @@ double integra(double a, double b) {
     return altura*largura;
 }
 
-int main() {
-    printf("%lf", integra(1,0.004f));
-
-    return 0;
+double integraIntervalo(double a, double b, double delta) {
+    double final = (a > b)? a : b;
+    double inicio = (b < a)? b : a;
+    double deltaInicio;
+    double total;
+    while (inicio < final) {
+        total += integra(inicio, inicio+delta);
+        inicio += delta;
+    }
+    
+    return total;
 }
+
+int main(void) {
+   int        comm_size;               
+   int        my_rank;
+   double     inicio, fim;
+   double     area, areaAux;
+  
+
+   MPI_Init(NULL, NULL); 
+   MPI_Comm_size(MPI_COMM_WORLD, &comm_size); 
+   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+   
+   inicio = (double) my_rank + 1;
+   fim = (double) inicio + 1;
+   //printf("%lf %lf \n", inicio, fim);
+   
+   area = integraIntervalo(inicio, fim, 0.000001);
+   printf("%lf \n", area);
+   
+   if (my_rank != 0) {
+      /* Envio da mensagem para o processo 0 */
+      MPI_Send(&area, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+   } else {  
+      
+      for (int q = 1; q < comm_size; q++) {
+         /* Recepção da mensagem do processo q */
+         MPI_Recv(&areaAux, 1, MPI_DOUBLE, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+         area += areaAux;
+      }
+      /* Impressão da mensagem do processo centralizador */
+      printf("A área é: %lf \n", area);
+   }
+
+   /* Finaliza MPI */
+   MPI_Finalize(); 
+
+   return 0;
+}  /* main */
