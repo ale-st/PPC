e4f8f7595da6c9ca91823f02b80cab30ada48a2b
Ale Santos <71612455+ale-st@users.noreply.github.com>
2024-08-01 22:11:33 -0300

    Update integral.c

--- MPI/Atividades/integral.c
+++ MPI/Atividades/integral.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
+// função q representa f(x)= x^2 - 4x + 8
 double func(double x) {
     return (double) pow(x,2)-4*x+8;
 }
 
+//função que calcula area aproximada de um retângulo, no intervalo [a,b] de func
 double integra(double a, double b) {
     double altura, largura;
     if (a > b) {
         return integra(b,a);
     }
     if (func(a) < func(b)) {
-        
+        altura = func(b);
     }
-    altura = func(b) - func(a);
+    else {
+        altura = func(a);
+    }
     largura = b - a;
     return altura*largura;
 }
 
 int main() {
-    printf("%lf", integra(1,5));
+    printf("%lf", integra(1,0.004f));
 
     return 0;
 }
